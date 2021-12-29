1ba00da40db60b1715886d6d0d01669cf88a7270
lewkamtao <div@kamtao.com>
2021-12-29 17:34:47 +0800

    🔧 build:dev and build

--- plugins/pre.js
+++ plugins/pre.js
@@ -1,4 +1,4 @@
-import VMdPreview from "@kangc/v-md-editor/lib/preview";
+import VMdPreview from "@kangc/v-md-editor/lib/preview.js";
 import "@kangc/v-md-editor/lib/style/preview.css";
 import githubTheme from "@kangc/v-md-editor/lib/theme/github.js";
 import "@kangc/v-md-editor/lib/theme/style/github.css";
