592f691bb9a68750d3b9a9ac958f5f25e4b12d24
lewkamtao <div@kamtao.com>
2021-12-29 16:33:30 +0800

    ✨ feat:port修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       args: "start",
       env: {
         NODE_ENV: "production",
-        PORT: 3093, // 修改之后 注意反向代理也要改!
+        PORT: 3092, // 修改之后 注意反向代理也要改!
       },
     },
   ],
