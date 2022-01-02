f6b693cb49de762a8761b8fdff6d29daafa2641c
卢锦涛 <div@kamtao.com>
2022-01-02 16:32:14 +0800

    🐞 fix:裁剪封面报错

--- components/base/GeekAvatarEdit.vue
+++ components/base/GeekAvatarEdit.vue
@@ -132,12 +132,12 @@ onMounted(() => {
   const geekAvatarEditUserInfo: any = document.getElementById(
     "geekAvatarEditUserInfo"
   );
+  init();
   //   初始化
   checkUserInfo();
   function checkUserInfo() {
     setTimeout(async () => {
       if (geekAvatarEditUserInfo.innerHTML) {
-        init();
         user.value = JSON.parse(geekAvatarEditUserInfo.innerHTML);
         cropper.value.replace(await util.getBase64(user.value.avatar), false); // 默认false，适应高度，不失真
       } else {
